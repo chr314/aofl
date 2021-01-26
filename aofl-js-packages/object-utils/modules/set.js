dcf6c15c06add0194042f0102160f0a4b412d670
Christoforos Aslanov <christoforos@christoforos.dev>
2021-01-26 02:04:25 +0200

    fix prototype pollution

--- aofl-js-packages/object-utils/modules/set.js
+++ aofl-js-packages/object-utils/modules/set.js
@@ -16,6 +16,9 @@ import {recurseObjectByPath} from './core';
  */
 const set = (obj, path, val) => {
   return recurseObjectByPath(obj, path, (key, pathParts, source, recurse) => {
+    if (key === '__proto__') {
+      return recurse(pathParts, source);
+    }
     if (pathParts.length === 0) {
       source[key] = val;
       return;
